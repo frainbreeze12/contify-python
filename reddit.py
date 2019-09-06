dc0ea44d6da4162217d1ee51f2b6902ca1e8c345
Marvin Dobslaw <marvin.dobslaw@sonepar.de>
2019-09-06 08:12:51 +0200

    changed dark theme, added r/poular

--- reddit.py
+++ reddit.py
@@ -1,17 +1,19 @@
 import feedparser
+from apscheduler.schedulers.background import BackgroundScheduler
 
 def get_rss():
     content = ""
-    redditList = ("http://www.reddit.com/r/news/.rss", "http://www.reddit.com/r/technology/.rss", "http://www.reddit.com/r/worldnews/.rss",
-                  "http://www.reddit.com/r/gaming/.rss", "http://www.reddit.com/r/Showerthoughts/.rss", "http://www.reddit.com/r/hongkong/.rss",
-                  "http://www.reddit.com/r/europe/.rss", "http://www.reddit.com/r/dankmemes/.rss", "http://www.reddit.com/r/KidsAreFuckingStupid/.rss")
+    redditList = ("http://www.reddit.com/r/news/.rss", "http://www.reddit.com/r/technology/.rss", "https://www.reddit.com/r/popular/.rss",
+                  "http://www.reddit.com/r/worldnews/.rss", "http://www.reddit.com/r/europe/.rss","http://www.reddit.com/r/hongkong/.rss",
+                  "http://www.reddit.com/r/gaming/.rss", "http://www.reddit.com/r/Showerthoughts/.rss", "http://www.reddit.com/r/KidsAreFuckingStupid/.rss",
+                  "http://www.reddit.com/r/dankmemes/.rss")
 
     for sub in redditList:
         d = feedparser.parse(sub)
         limit = 1
 
         content += '<div class="col s12 m6 xl4"><ul class="collection with-header z-depth-1 hoverable">'
-        content += '<li class="collection-header"> <h5>{}</h5></li>'.format(d['feed']["tags"][0].label)
+        content += '<li class="collection-header"> <h6>{}</h6></li>'.format(d['feed']["tags"][0].label)
         for post in d.entries:
             content += '<li class="collection-item truncate"><a href="{}">{}</a></li>'.format(post.link, post.title)
             if limit == 10:
@@ -19,4 +21,8 @@ def get_rss():
             limit+=1
         content += '</ul></div>'
 
-    return content 
+    return content
+
+sched = BackgroundScheduler()
+sched.add_job(get_rss, 'interval', minutes=5)
+sched.start()
