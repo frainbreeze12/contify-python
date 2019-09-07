8b640b4614928641c98f7d8ccd7da5904d068b85
Marvin Dobslaw <dobslaw.marvin@outlook.de>
2019-09-07 16:18:27 +0200

    added more categories

--- server.py
+++ server.py
@@ -11,9 +11,17 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 def index():
     return render_template("summary.html")
 
-@app.route('/quellen')
-def quellen():
-    return render_template("source.html")
+@app.route('/wirtschaft')
+def wirtschaft():
+    return render_template("wirtschaft.html")
+
+@app.route('/politik')
+def politik():
+    return render_template("politik.html")
+
+@app.route('/technologie')
+def technologie():
+    return render_template("technologie.html")
 
 @app.route('/subreddit')
 def reddit():
