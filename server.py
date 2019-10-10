31d3f4f38a799b8cf194e4f7e7ba2bb00117f250
Marvin Dobslaw <dobslaw.marvin@outlook.de>
2019-10-10 19:28:20 +0200

    added 404 page

--- server.py
+++ server.py
@@ -8,26 +8,37 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 rss_links()
 
+
 @app.route('/')
 def index():
     return render_template("home.html")
+
 
 @app.route('/economy')
 def wirtschaft():
     return render_template("economy.html")
 
+
 @app.route('/politics')
 def politik():
     return render_template("politics.html")
+
 
 @app.route('/tech&science')
 def technologie():
     return render_template("technology.html")
 
+
 @app.route('/security')
 def security():
-    return render_template("security.html")    
+    return render_template("security.html")
+
 
 @app.route('/subreddit')
 def reddit():
     return render_template("subreddit.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
