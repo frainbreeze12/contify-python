46187b25474202ec572f17a42772138bad150061
Marvin Dobslaw <marvin.dobslaw@sonepar.de>
2019-10-14 11:30:29 +0200

    added more news, changed background scheduler to 30mins

--- rss_links.py
+++ rss_links.py
@@ -6,14 +6,17 @@ def rss_links():
     economy_links   = ("http://www.makronom.de/feed", "https://www.gruenderszene.de/feed", "https://www.wiwo.de/contentexport/feed/rss/schlagzeilen",
                        "https://www.businessinsider.de/rss", "https://www.handelsblatt.com/contentexport/feed/wirtschaft", "http://www.manager-magazin.de/news/index.rss")
 
-    politic_links   = ("https://www.n-tv.de/politik/rss", "https://www.handelsblatt.com/contentexport/feed/politik", "https://www.dgb.de/@@rss?count=20&feed=9ba1995a-0fe3-11df-7728-00093d10fae2")
+    politic_links   = ("https://www.n-tv.de/politik/rss", "https://www.handelsblatt.com/contentexport/feed/politik", "https://www.dgb.de/@@rss?count=20&feed=9ba1995a-0fe3-11df-7728-00093d10fae2",
+                       "https://netzpolitik.org/feed")
 
     tech_links      = ("https://www.heise.de/rss/heise-top-atom.xml", "https://rss.golem.de/rss.php?feed=ATOM1.0", "https://news.ycombinator.com/rss",
+                       "https://www.wired.com/feed/rss", "http://rss.slashdot.org/Slashdot/slashdotMainatom", "https://feeds.howtogeek.com/howtogeek/features", 
                        "https://www.eff.org/rss/updates.xml", "https://www.techrepublic.com/rssfeeds/articles/", "https://www.nytimes.com/svc/collections/v1/publish/http://www.nytimes.com/section/science/rss.xml")
 
     security_links  = ("https://www.heise.de/security/rss/news-atom.xml", "https://www.heise.de/security/rss/alert-news-atom.xml", "https://www.security-insider.de/rss/news.xml",
                        "https://threatpost.com/feed/", "http://feeds.feedburner.com/Securityweek?format=xml", "https://feeds.feedburner.com/TheHackersNews", 
-                       "https://www.darkreading.com/rss_simple.asp", "https://cyware.com/allnews/feed", "https://www.securitymagazine.com/rss/topic/2236")
+                       "https://krebsonsecurity.com/feed/", "https://www.darkreading.com/rss_simple.asp", "https://cyware.com/allnews/feed", 
+                       "https://www.securitymagazine.com/rss/topic/2236")
 
     reddit_links    = ("http://www.reddit.com/r/popular/.rss", "http://www.reddit.com/r/technology/.rss", "https://www.reddit.com/r/news/.rss",
                        "http://www.reddit.com/r/europe/.rss","http://www.reddit.com/r/hongkong/.rss", "http://www.reddit.com/r/gaming/.rss",
@@ -28,5 +31,5 @@ def rss_links():
 
 
 sched = BackgroundScheduler()
-sched.add_job(rss_links, 'interval', minutes=5)
+sched.add_job(rss_links, 'interval', minutes=30)
 sched.start()
