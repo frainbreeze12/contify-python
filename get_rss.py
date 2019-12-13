58d6b01cf8d44d57731b9b4cf9cd072da0bdad90
Marvin Dobslaw <marvin.dobslaw@sonepar.de>
2019-12-13 12:48:11 +0100

    more audit fixes

--- get_rss.py
+++ get_rss.py
@@ -11,7 +11,7 @@ def get_rss(links, name):
     feedList = links
 
     if(name == "home"):
-        allStuff += '<h4 class="center-align">Help Contify grow!</h4> <p class="center-align">Feel free to submit a new news source via a github issue! Please label them accordingly with a content label <a href="https://github.com/frainbreeze12/contify-python/issues">here</a>.</p>'
+        allStuff += '<h4 class="center-align">Help Contify grow!</h4> <p class="center-align">Feel free to submit a new news source via a github issue! Please label them accordingly with a content label here: <a href="https://github.com/frainbreeze12/contify-python/issues" aria-label="Link to GitHub issues" rel="noopener">GitHub issues</a>.</p>'
 
     for feed in feedList:
         d = feedparser.parse(feed)
@@ -25,11 +25,11 @@ def get_rss(links, name):
             print("fetching feed from: {}".format(feed))
 
             if(d.feed.title == ""):
-                allStuff += '<li class="collection-header"><h6><a href="{}">WirtschaftsWoche</a></h6></li>\n'.format(d.feed.link)
+                allStuff += '<li class="collection-header"><h6><a href="{}" rel="noopener">WirtschaftsWoche</a></h6></li>\n'.format(d.feed.link)
             else:
-                allStuff += '<li class="collection-header"><h6><a href="{}">{}</a></h6></li>\n'.format(d.feed.link ,d.feed.title)
+                allStuff += '<li class="collection-header"><h6><a href="{}" rel="noopener">{}</a></h6></li>\n'.format(d.feed.link ,d.feed.title)
             for post in d.entries:
-                allStuff += '<li class="collection-item"><a href="{}" target="_blank" class="truncate">{}</a></li>\n'.format(post.link, post.title)
+                allStuff += '<li class="collection-item"><a href="{}" target="_blank" rel="noopener" class="truncate">{}</a></li>\n'.format(post.link, post.title)
                 if limit == 10:
                     break
                 limit+=1
